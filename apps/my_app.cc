15f3d58183ea2efd5f2207f10f37f90c27bf7094
riyv <riyag3@illinois.edu>
2020-04-21 19:27:32 -0500

    Created ball and two paddles

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -3,19 +3,65 @@
 #include "my_app.h"
 
 #include <cinder/app/App.h>
+#include <iostream>
+#include "cinder/app/RendererGL.h"
+#include "cinder/gl/gl.h"
+#include <cinder/Font.h>
+#include <cinder/Text.h>
+#include <cinder/Vector.h>
+#include <cinder/audio/audio.h>
+#include <cinder/audio/Voice.h>
+#include <cinder/gl/draw.h>
+#include <cinder/gl/gl.h>
 
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <string>
+#include "cinder/Rand.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
 
 namespace myapp {
 
 using cinder::app::KeyEvent;
 
-MyApp::MyApp() { }
+MyApp::MyApp() {
 
-void MyApp::setup() { }
+//    // initialize opengl (via glut)
+//    glutInit(&argc, argv);
+//    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+//    glutInitWindowSize(width, height);
+//    glutCreateWindow("noobtuts.com Pong");
+//
+//    // start the whole thing
+//    glutMainLoop();
+   // return 0;
+
+}
+
+
+void MyApp::setup() {
+  // cout<< "Hello World"<<endl;
+
+}
 
 void MyApp::update() { }
 
-void MyApp::draw() { }
+void MyApp::draw() {
+
+  gl::clear();
+  vec2 center = getWindowCenter();
+  float r = 20;
+
+  gl::color( Color( 1, 0, 0 ) ); // red
+  gl::drawSolidCircle( center + vec2( -r, r ), r );
+
+  drawRect(racket_left_x, racket_left_y, racket_width,  racket_height);
+  drawRect(racket_right_x, racket_right_y,  racket_width,  racket_height);
+}
 
 void MyApp::keyDown(KeyEvent event) { }
 
--- apps/my_app.h
+++ apps/my_app.h
@@ -4,11 +4,46 @@
 #define FINALPROJECT_APPS_MYAPP_H_
 
 #include <cinder/app/App.h>
-
+#include <cinder/gl/gl.h>
+#include <cinder/audio/audio.h>
+#include <cinder/audio/Voice.h>
 
 namespace myapp {
+using cinder::Color;
+using cinder::ColorA;
+using cinder::Rectf;
+using cinder::TextBox;
+using cinder::app::KeyEvent;
+using cinder::audio::VoiceRef;
+using std::chrono::duration_cast;
+using std::chrono::seconds;
+using std::chrono::system_clock;
+using std::string;
 
 class MyApp : public cinder::app::App {
+
+  // rackets in general
+  int racket_width = 10;
+  int racket_height = 80;
+  int racket_speed = 3;
+
+// left racket position
+  float racket_left_x = 10.0f;
+  float racket_left_y = 400.0f;
+
+// right racket position
+  float racket_right_x = 800 - racket_width - 10; // Later replace 800 by myapp::kWidth
+  float racket_right_y = 400.0f;
+
+  void drawRect(float x, float y, float width, float height) {
+
+    cinder::gl::color(Color(0, 1, 0));
+    cinder::gl::drawSolidRect(Rectf(x,
+                                    y,
+                                    x+width,
+                                    y+height));
+  }
+
  public:
   MyApp();
   void setup() override;
